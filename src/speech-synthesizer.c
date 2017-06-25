3e0f0a38e4a4eccdb95b128b5ed814a15156d09f
plastictesseract <michael.rosa.qwerty@gmail.com>
2017-06-25 12:39:33 -0400

    Comments and ending allophone
    Ends on a pause automatically to prevent holding a sound for two seconds

--- src/speech-synthesizer.c
+++ src/speech-synthesizer.c
@@ -13,41 +13,45 @@
 #include "allophones.h"
 #include "words.h"
 
-#define  DO    PB1
-#define  USCK  PB2
-#define  ALD   PB3
-#define  LRQ   PB4
+#define  DOUT  PB1       // serial data out
+#define  USCK  PB2       // serial clock out
+#define  ALD   PB3       // address load, output, pulsed low to load allophone
+#define  LRQ   PB4       // load request, input, pulled low when the next allophone can be loaded
 
 uint8_t phrase[] = {HELLO, PA5, I, PA3, AM, PA3, THE, PA3,
                     GENERAL, PA3, INSTRUMENT, PA3,
                     S, P, ZERO, TWO, FIFTY, SIX, PA4, 
-                    VARIANT, PA3, A, L, TWO, PA1};
+                    VARIANT, PA3, A, L, TWO};
 
 
 void send(const uint8_t alloph)
 {
-  USIDR = alloph;
-  USISR = 1 << USIOIF;
-  while(!(USISR & 1 << USIOIF)) {USICR |= 1 << USITC;} 
-  PORTB &= ~(1 << ALD); ;
+  USIDR = alloph;           // put allophone in data register
+  USISR |= 1 << USIOIF;     // clear counter overflow interrupt flag
+  while(!(USISR & 1 << USIOIF)) 
+    {USICR |= 1 << USITC;}  // output the allohphone & clock
+  PORTB &= ~(1 << ALD);     // pulse ALD line low to load allophone
   _delay_us(1); 
   PORTB |= 1 << ALD;
 }
 
 int main(void)
 {
-  USICR |= 1 << USIWM0 | 1 << USICS1 | 1 << USICLK;
-  DDRB  |= 1 << DO | 1 << USCK | 1 << ALD;
+  USICR |= 1 << USIWM0 | 1 << USICS1 | 1 << USICLK;   // SPI (3-wire) mode, external clock w/ software strobe
   
+  DDRB  |= 1 << DOUT | 1 << USCK | 1 << ALD; 
   PORTB |= 1 << ALD;
   
   while(1)
   {
     for(int i = 0; i < sizeof(phrase)/sizeof(uint8_t); i++)
     {
-      while(PINB & 1 << LRQ){}
-      send(phrase[i]); 
+      while(PINB & 1 << LRQ){}    // wait until the chip is ready
+      send(phrase[i]);            // send the allophone
     }
+    
+    while(PINB & 1 << LRQ){}
+    send(PA1);                    // end with silence instead of holding last allophone
     
     _delay_ms(2000);
   }
